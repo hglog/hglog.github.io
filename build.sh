198b2ebba09434b613586c19ac616bd1ed5cdedb
hglog <gallenoffice@gmail.com>
2026-04-17 19:14:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # npm install
-# git clone https://github.com/GallenHu/hexo-theme-light.git themes/light --depth=1
-git clone https://github.com/GallenHu/hexo-theme-minos.git themes/minos
+# git clone https://github.com/Glenhoooo/hexo-theme-light.git themes/light --depth=1
+git clone https://github.com/Glenhoooo/hexo-theme-minos.git themes/minos
 cp themes/minos/_config.example.yml themes/minos/_config.yml
 node init_script.js
 
